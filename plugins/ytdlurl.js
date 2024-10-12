31c532816ee8863115242fd1835204bc0ede53d2
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-10-12 15:16:41 +0530

    Update ytdlurl.js

--- plugins/ytdlurl.js
+++ plugins/ytdlurl.js
@@ -1,1 +1,20 @@
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
+cmd({
+        pattern: "yt",
+        desc: "Sends a fact in chat.",
+        category: "downloder",
+        react :"🔮",
+        filename: __filename,
+    },
+    async (conn, mek, m, {
+        from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, 
+        botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, 
+        participants, groupAdmins, isBotAdmins, isAdmins, reply
+    }) => {
+        const { data } = await axios.get(`https://temp.giftedapis.us.kg/api/download/ytdl?url={q}?feature=shared&apikey=gifted`);
+        return reply(`*Fact:* ${data.title}\n\n> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺`);
+    }
+);
 
