12cc2f8d83f50e330dce795fc0f21601768096e0
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-03 07:33:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 module.exports = {
 
 //╭──────────────────────Main──────────────────────╮//   
-    SESSION_ID: process.env.SESSION_ID || "ELIXAMD❤️8BBFQbBS#-fZUOkYFMFzMo5dM9JCWZJNaaQIjrHspS_KMnNOSVk8",
+    SESSION_ID: process.env.SESSION_ID || "8BBFQbBS#-fZUOkYFMFzMo5dM9JCWZJNaaQIjrHspS_KMnNOSVk8",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Eboxsl/ELAUTO/refs/heads/main/Elixa/menu.png",
     ALIVE_MSG: process.env.ALIVE_MSG || "Elixs Is Alive Nox👍🇱🇰✅ ",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
