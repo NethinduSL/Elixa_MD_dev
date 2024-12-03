ba8b753a1a5aab3f07874da24a9c5f6faca0eb03
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-03 10:46:48 +0530

    Update tools.js

--- plugins/tools.js
+++ plugins/tools.js
@@ -1,23 +1,24 @@
 const config = require('../config');
 const { cmd, commands } = require('../command');
-const { fetchJson } = require('../lib/functions');
+const { fetchJson, getBuffer } = require('../lib/functions');
 
-
-
-cmd 
-({
+cmd({
     pattern: "qr",
     category: "owner",
     filename: __filename,
-    desc: "Sends CitelsVoid QR code to scan and get your session ID."
+    desc: "Sends QR code of text"
 },
 async (Void, citel, text) => {
-    if (!text) return citel.reply("Please provide text to generate the QR code.");
-    let qrelixa = await getBuffer(`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${q}`);
-    await Void.sendMessage(citel.chat, { image: qrelixa });
-    
+    try {
+        if (!text) return citel.reply("Please provide text to generate the QR code.");
+        let qrelixa = await getBuffer(`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${text}`);
+        
+        await Void.sendMessage(citel.chat, {
+            image: qrelixa,
+            caption: "> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺",
+        }, { quoted: citel });
+    } catch (e) {
+        console.error(e);
+        citel.reply("An error occurred while generating the QR code. Please try again.");
+    }
 });
-
-
-
-
