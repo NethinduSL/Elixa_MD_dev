f2111f60a751ef10d2fa30c1131638f6bde5d8c1
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-08 16:15:48 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -3,7 +3,7 @@ const { cmd, commands } = require('../command');
 const { fetchJson } = require('../lib/functions');
 
 cmd({
-    pattern: "ai",
+    pattern: "gemini",
     desc: "Chat GPT 4.",
     category: "ai",
     react :"💫",
@@ -12,7 +12,61 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         let data = await fetchJson(`https://bk9.fun/ai/gemini?q=${q}`);
-        return reply(`${data.BK9}`);
+        return reply(`${data.BK9}\n\n> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺`);
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
+
+
+cmd({
+    pattern: "llama",
+    desc: "Chat GPT 4.",
+    category: "ai",
+    react :"💫",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        let data = await fetchJson(`https://bk9.fun/ai/llama?q=${q}`);
+        return reply(`${data.BK9}\n\n> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺`);
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
+
+
+cmd({
+    pattern: "jeeves",
+    desc: "Chat GPT 4.",
+    category: "ai",
+    react :"💫",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        let data = await fetchJson(`https://bk9.fun/ai/blackbox?q=${q}`);
+        return reply(`${data.BK9}\n\n> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺`);
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
+
+
+cmd({
+    pattern: "blackbox",
+    desc: "Chat GPT 4.",
+    category: "ai",
+    react :"💫",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        let data = await fetchJson(`https://bk9.fun/ai/jeeves-chat?q=${q}`);
+        return reply(`${data.BK9}\n\n> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺`);
     } catch (e) {
         console.log(e);
         reply(`${e}`);
