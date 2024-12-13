877725f8f4c4e2ad55230c32294809c924d2f0eb
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-13 07:27:20 +0530

    Update data.js

--- plugins/data.js
+++ plugins/data.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 const { cmd, commands } = require('../command');
 
 cmd({
-    pattern: "nt",
+    pattern: "nethindu",
     desc: "",
-    category: "downloade",
+    category: "owner",
     react: "✅",
     filename: __filename,
 }, async (conn, mek, m, {
@@ -21,11 +21,45 @@ cmd({
         const ntValue = response.data.NT;  // Ensure this is correct based on the response structure
         
         // Send the NT value in the reply
-        await reply(`NT Value: ${ntValue}\n\n> 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 𝗯𝘆 𝗘𝗹𝗶𝘇𝗮 ‐𝝡𝗗༺`);
+        await reply(`${ntValue}\n\n> 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 𝗯𝘆 𝗘𝗹𝗶𝘇𝗮 ‐𝝡𝗗༺`);
         
         // React to the message
         await conn.sendMessage(from, {
             react: { text: '🖥️', key: mek.key }
+        });
+
+    } catch (error) {
+        console.error("Error fetching data:", error.message);  // Log the error for debugging
+        await reply("An error occurred while processing the request.");
+    }
+});
+
+
+cmd({
+    pattern: "jithula",
+    desc: "",
+    category: "owner",
+    react: "✅",
+    filename: __filename,
+}, async (conn, mek, m, {
+    from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, 
+    botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, 
+    participants, groupAdmins, isBotAdmins, isAdmins, reply
+}) => {
+
+    try {
+        // Fetching data from the GitHub repository
+        const response = await axios.get('https://raw.githubusercontent.com/Eboxsl/ELAUTO/refs/heads/main/publicconfig.js');
+        
+        // Access the NT value from the response data
+        const jbValue = response.data.JB;  // Ensure this is correct based on the response structure
+        
+        // Send the NT value in the reply
+        await reply(`${jbValue}\n\n> 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 𝗯𝘆 𝗘𝗹𝗶𝘇𝗮 ‐𝝡𝗗༺`);
+        
+        // React to the message
+        await conn.sendMessage(from, {
+            react: { text: '❤️', key: mek.key }
         });
 
     } catch (error) {
