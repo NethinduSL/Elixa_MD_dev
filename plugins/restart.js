925e2019f9049f72bec06a77fdcf892ef62ec8ac
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-13 07:31:41 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -19,3 +19,23 @@ console.log(e)
 reply(`${e}`)
 }
 })
+
+
+
+cmd({
+    pattern: "update",
+    desc: "Update the Elix",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const {exec} = require("child_process")
+reply("Afeter Restart send .updatel")
+await sleep(1500)
+exec("pm2 restart all")
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
