4c02b6dd20417d9ab3cc8c8d09a171a2cb6d4ab1
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-14 08:47:23 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,4 +1,4 @@
-/*const config = require('../config');
+const config = require('../config');
 const { cmd, commands } = require('../command');
 const { fetchJson, getBuffer } = require('../lib/functions');
 
