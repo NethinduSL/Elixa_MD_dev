4aa0205c04d1f46a7b803cd7e40c127730257c67
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-22 21:57:21 +0530

    Update Ytdl.js

--- plugins/Ytdl.js
+++ plugins/Ytdl.js
@@ -67,3 +67,71 @@ async (conn, mek, m, {
         reply(`Error: ${e.message || "Something went wrong 🙃"}`);
     }
 });
+
+
+
+
+cmd({
+    pattern: "video2",
+    desc: "Download video By Elixa.",
+    category: "download",
+    react: "🎬",
+    filename: __filename
+},
+async (conn, mek, m, {
+    from, quoted, q, reply
+}) => {
+    
+        try {
+        if (!q) return reply("Please provide a valid URL 🙃");
+        const search = await yts(q);
+        const data = search.videos[0];
+        const url = data.url;
+
+        let desc = `
+╭❰𝗘ꟾ𝖎✘𝗮 𝗠𝗗 𝗩𝗜𝗗𝗘𝗢🎬 ❱❱
+│
+╰📌𝗧𝗶𝘁𝗹𝗲: ${data.title}
+╰🔗𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${data.description}
+╰🕦𝗧𝗶𝗺𝗲: ${data.timestamp}
+╰📤𝗔𝗴𝗼: ${data.ago}
+╰👁️𝗩𝗶𝗲𝘄𝘀: ${data.views}
+┃ɪꜰ ɴᴏᴛ ᴡᴏʀᴋ ᴜꜱᴇ ᴠɪᴅᴇᴏ
+╰═══════════════
+
+> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺
+`;
+
+        await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
+
+        // Use your API to get the MP3 download URL
+        const apiUrl = `https://api.giftedtech.my.id/api/download/ytmp4?apikey=gifted&url=${url}`;
+        const response = await axios.get(apiUrl);
+        const data2 = response.data;
+
+        if (!data2.success) {
+            return reply("Failed to download the video 🙃");
+        }
+
+        const { download_url } = data2.result;
+
+        // Send audio as a playable file
+        await conn.sendMessage(from, {
+            audio: { url: download_url },
+            mimetype: "video/mp4",
+            fileName: `${title}.mp4`
+        }, { quoted: mek });
+
+        // Send audio as a downloadable document
+        await conn.sendMessage(from, {
+            document: { url: download_url },
+            mimetype: "video/mp4",
+            fileName: `${title}.mp4`,
+            caption: "®𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗"
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.error(e);
+        reply(`Error: ${e.message || "Something went wrong 🙃"}`);
+    }
+});
