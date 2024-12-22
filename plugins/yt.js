af75c8eef6cacfbeb409c738f2c9ffc59a133524
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-22 20:38:28 +0530

    Create yt.js

---
+++ plugins/yt.js
@@ -0,0 +1,110 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const fg = require('api-dylux');
+const yts = require('yt-search');
+const axios = require('axios');
+const { fetchJson } = require('../lib/functions');
+
+const { Buffer } = require('buffer');
+
+const gifted = require('gifted-dls');
+
+cmd({
+    pattern: "song2",
+    desc: "Download Songs By Elixa.",
+    category: "download",
+    react : "🎵",
+    filename: __filename
+},
+async (conn, mek, m, {
+    from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply
+}) => {
+    try {
+        if (!q) return reply("Please provide a valid URL 🙃");
+        const search = await yts(q);
+        const data = search.videos[0];
+        const url = data.url;
+
+        let desc = `
+╭❰𝗘ꟾ𝖎✘𝗮 𝗠𝗗 𝗦𝗼𝗻𝗴 🎵 ❱❱
+│
+╰📌𝗧𝗶𝘁𝗹𝗲: ${data.title}
+╰🔗𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${data.description}
+╰🕦𝗧𝗶𝗺𝗲: ${data.timestamp}
+╰📤𝗔𝗴𝗼: ${data.ago}
+╰👁️𝗩𝗶𝗲𝘄𝘀: ${data.views}
+╰═══════════════
+
+> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺
+`;
+
+        await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
+
+        // download audio
+        let data = await gifted.giftedytmp3(url)
+        let downloadUrl = down.audio_buffer;
+
+        // send audio
+        await conn.sendMessage(from, { audio: { url: downloadUrl }, mimetype: "audio/mpeg" }, { quoted: mek });
+        await conn.sendMessage(from, { document: { url: downloadUrl }, mimetype: "audio/mp3", fileName: data.title + ".mp3", caption: "®𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗" }, { quoted: mek });
+
+    } catch (e) {
+        console.log(e);
+        reply(`Error: ${e}`);
+    }
+});
+
+//---------------------------video--------------------------
+
+cmd({
+    pattern: "video2",
+    desc: "Download videos By Elixa.",
+    category: "download",
+    react :"🎬",
+    filename: __filename
+},
+async (conn, mek, m, {
+    from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply
+}) => {
+    try {
+        if (!q) return reply("Please provide a valid URL 🙃");
+        const search = await yts(q);
+        const data = search.videos[0];
+        const url = data.url;
+
+        let desc = `        
+╭❰𝗘ꟾ𝖎✘𝗮 𝗠𝗗 𝗩𝗶𝗱𝗲𝗼 🎬❱❱
+┃
+╰📌𝗧𝗶𝘁𝗹𝗲: ${data.title}
+╰🔗𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${data.description}
+╰🕦𝗧𝗶𝗺𝗲: ${data.timestamp}
+╰📤𝗔𝗴𝗼: ${data.ago}
+╰👁️𝗩𝗶𝗲𝘄𝘀: ${data.views}
+╰═══════════════
+
+> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺
+`;
+
+        await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
+
+        // download video
+        let down = await fg.ytv(url); // Use the video's URL for downloading
+        let downloadUrl = down.dl_url;
+
+        // send video
+        await conn.sendMessage(from, { video: { url: downloadUrl }, mimetype: "video/mp4" }, { quoted: mek });
+        await conn.sendMessage(from, { document: { url: downloadUrl }, mimetype: "video/mp4", fileName: data.title + ".mp4", caption: "®𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗" }, { quoted: mek });
+
+    } catch (e) {
+        console.log(e);
+        reply(`Error: ${e}`);
+    }
+});
+
+
+
+
+
+
+
+
